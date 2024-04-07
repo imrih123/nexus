af92371a40c3a84f40627285c3fd4419365c6d36
imrih123 <150593844+imrih123@users.noreply.github.com>
2024-04-07 11:28:26 +0300

    Add files via upload

--- nexus-main/allcode/settingCli.py
+++ nexus-main/allcode/settingCli.py
@@ -1,7 +1,9 @@
-SERVER_IP = '192.168.4.81'
+SERVER_IP = '192.168.4.96'
 GENERAL_PORT = 1500
 NITUR_PORT = 1600
 P2P_PORT = 3333
+BYTES_PER_SECOND = 19000000
+time_to_connect = 0.5
 PATH_TO_SAVE_FILES = fr"C:\Users\talmid\Downloads"
 NITUR_FOLDER = fr"D:\.nitur"
 IMAGES_PATH = fr"T:\public\יב\imri\nexus-main\\"
